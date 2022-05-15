86f01c3b24b5dd04a909742346297c113bf4d4a0
Ange1D <47231898+Ange1D@users.noreply.github.com>
2022-05-14 20:47:55 -0500

    Corregido error al construir(build:prod)

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('SafeFishing');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('SafeFishing app is running!');
-  });
 });
