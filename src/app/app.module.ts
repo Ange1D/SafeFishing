4d82f2b4731fc38f378d86977c4b52da4bd3bfb7
Ange1D <47231898+Ange1D@users.noreply.github.com>
2022-05-14 20:31:12 -0500

    Revert "Merge branch 'main' of https://github.com/Ange1D/SafeFishing"
    This reverts commit 8b616730c3efff2cf7f648c6fc6d500e41355a8f, reversing
    changes made to 2bf78af107a7d03c835257ebee407fe45c325e32.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,26 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatListModule} from '@angular/material/list';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-
-import { MenuComponent } from './components/menu/menu.component';
-
 @NgModule({
   declarations: [
-    AppComponent,
-    MenuComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    MatSidenavModule,
-    MatListModule,
-    MatToolbarModule,
-    MatIconModule
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
